3ae2af8bb56223142f5b901da94db7d938f8aeaa
Ta-SeenJunaid <taseen.junaid@gmail.com>
2021-05-10 07:27:53 +0600

    sender.py

---
+++ sender.py
@@ -0,0 +1,14 @@
+import socket
+import time
+from imutils.video import VideoStream
+import imagezmq
+
+#use your own receiver_server address
+sender = imagezmq.ImageSender(connect_to='tcp://192.168.68.107:5555')
+
+sender_name = socket.gethostname()
+cam = VideoStream(0).start()
+time.sleep(2.0)
+while True:
+    image = cam.read()
+    sender.send_image(sender_name, image)
