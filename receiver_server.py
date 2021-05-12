807bc355beefed466d81f0b0e453c081005d5bfd
Ta-SeenJunaid <taseen.junaid@gmail.com>
2021-05-12 06:23:05 +0600

    Face detection & recognition

--- receiver_server.py
+++ receiver_server.py
@@ -1,6 +1,7 @@
 import cv2
 import face_recognition
 import imagezmq
+import numpy as np
 import os
 
 path = 'image_attendance'
@@ -9,16 +10,39 @@ class_names = []
 my_list = os.listdir(path)
 
 
+def process_image(image, sender_name):
+    image = cv2.flip(image, 1)
+
+    img_small = cv2.resize(image, (0,0), None, 0.25, 0.25)
+    img_small = cv2.cvtColor(img_small, cv2.COLOR_BGR2RGB)
+
+    faces_current_frame = face_recognition.face_locations(img_small)
+    encodes_current_frame = face_recognition.face_encodings(img_small, faces_current_frame)
+
+    for encode_face, face_loc in zip(encodes_current_frame, faces_current_frame):
+        matches = face_recognition.compare_faces(encode_list_known, encode_face)
+        face_dis = face_recognition.face_distance(encode_list_known, encode_face)
+        # print(face_dis)
+        match_index = np.argmin(face_dis)
+
+        if matches[match_index]:
+            name = class_names[match_index].upper()
+            # print(name)
+            y1, x2, y2, x1 = face_loc
+            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(image, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(image, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            mark_attendance(name)
+
+    cv2.imshow(sender_name, image)
+
+
 for cl in my_list:
     cur_img = cv2.imread(f'{path}/{cl}')
     images.append(cur_img)
     class_names.append(os.path.splitext(cl)[0])
 print(class_names)
-
-
-def process_image(image, sender_name):
-    image = cv2.flip(image, 1)
-    cv2.imshow(sender_name, image)
 
 
 def find_encodings(images):
@@ -30,9 +54,11 @@ def find_encodings(images):
     return encode_list
 
 
+def mark_attendance(name):
+    pass
+
 encode_list_known = find_encodings(images)
 print('Encoding Complete')
-
 
 image_hub = imagezmq.ImageHub()
 
