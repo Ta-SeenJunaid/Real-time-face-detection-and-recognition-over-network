9e737813f016786300642eb67094b69f955ed644
Ta-SeenJunaid <taseen.junaid@gmail.com>
2021-11-20 11:54:58 +0100

    argument parsing for receiver_server

--- receiver_server.py
+++ receiver_server.py
@@ -4,12 +4,29 @@ import imagezmq
 import numpy as np
 import os
 from datetime import datetime
+import argparse
 
 
-path = 'image_attendance'
-images = []
-class_names = []
-my_list = os.listdir(path)
+def find_encodings(images):
+    encode_list = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encode_list.append(encode)
+    return encode_list
+
+
+def mark_attendance(name):
+    with open('attendance.csv', 'r+') as f:
+        my_data_list = f.readlines()
+        name_list = []
+        for line in my_data_list:
+            entry = line.split(',')
+            name_list.append(entry[0])
+        if name not in name_list:
+            now = datetime.now()
+            dt_string = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{dt_string}')
 
 
 def process_image(image, sender_name):
@@ -39,43 +56,43 @@ def process_image(image, sender_name):
 
     cv2.imshow(sender_name, image)
 
+if __name__ == "__main__":
 
-for cl in my_list:
-    cur_img = cv2.imread(f'{path}/{cl}')
-    images.append(cur_img)
-    class_names.append(os.path.splitext(cl)[0])
-print(class_names)
+    parser = argparse.ArgumentParser(
+        description="This is a receiver which will receive"
+                    "real time video data from senders")
+
+    parser.add_argument('--receiver_ip', required=False,
+                        help='please provide the receiver IP, '
+                             'example: --receiver_ip \'tcp://192.168.0.101:5555\', '
+                             'default=\'tcp://192.168.0.101:5555\'',
+                        type=str,
+                        default='tcp://192.168.0.101:5555')
+
+    args = parser.parse_args()
+
+    path = 'image_attendance'
+    images = []
+    class_names = []
+    my_list = os.listdir(path)
+
+    for cl in my_list:
+        cur_img = cv2.imread(f'{path}/{cl}')
+        images.append(cur_img)
+        class_names.append(os.path.splitext(cl)[0])
+    print(class_names)
+
+    encode_list_known = find_encodings(images)
+    print('Encoding Complete')
+
+    image_hub = imagezmq.ImageHub(open_port=args.receiver_ip)
+
+    while True:
+        sender_name, image = image_hub.recv_image()
+        process_image(image, sender_name)
+        image_hub.send_reply(b'ok')
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 
-def find_encodings(images):
-    encode_list = []
-    for img in images:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        encode = face_recognition.face_encodings(img)[0]
-        encode_list.append(encode)
-    return encode_list
 
-
-def mark_attendance(name):
-    with open('attendance.csv', 'r+') as f:
-        my_data_list = f.readlines()
-        name_list = []
-        for line in my_data_list:
-            entry = line.split(',')
-            name_list.append(entry[0])
-        if name not in name_list:
-            now = datetime.now()
-            dt_string = now.strftime('%H:%M:%S')
-            f.writelines(f'\n{name},{dt_string}')
-
-encode_list_known = find_encodings(images)
-print('Encoding Complete')
-
-image_hub = imagezmq.ImageHub()
-
-while True:
-    sender_name, image = image_hub.recv_image()
-    process_image(image, sender_name)
-    image_hub.send_reply(b'ok')
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
